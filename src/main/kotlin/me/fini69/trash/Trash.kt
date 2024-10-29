b982608689b6d516ee6a3fd0ac8780249bcfd697
Jakub Robaszek <112584909+JakubRobaszek@users.noreply.github.com>
2024-10-29 22:18:13 +0100

    Initial commit

---
+++ src/main/kotlin/me/fini69/trash/Trash.kt
@@ -0,0 +1,14 @@
+package me.fini69.trash
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class Trash : JavaPlugin() {
+
+    override fun onEnable() {
+        // Plugin startup logic
+    }
+
+    override fun onDisable() {
+        // Plugin shutdown logic
+    }
+}
