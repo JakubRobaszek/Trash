8c61d439570f3c0c2253b2c94fb52703a410d959
Jakub Robaszek <112584909+JakubRobaszek@users.noreply.github.com>
2024-10-30 00:31:39 +0100

    Fixing errors on older versions

--- src/main/kotlin/me/fini69/trash/listeners/InventoryListener.kt
+++ src/main/kotlin/me/fini69/trash/listeners/InventoryListener.kt
@@ -1,17 +1,31 @@
 package me.fini69.trash.listeners
 
 import me.fini69.trash.Trash
+import org.bukkit.ChatColor
 import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
 import org.bukkit.event.inventory.InventoryCloseEvent
+import java.lang.reflect.Method
 
-@Suppress("DEPRECATION")
-class InventoryListener(private val plugin: Trash): Listener {
+class InventoryListener(private val plugin: Trash) : Listener {
+    // Reflection method to get the title of an inventory view (we are checking this because the method is not available in all versions)
+    private val getTitleMethod: Method? = try {
+        val method = Class.forName("org.bukkit.inventory.InventoryView").getMethod("getTitle")
+        // Make the method accessible
+        method.isAccessible = true
+        method
+    } catch (e: Exception) {
+        // If the method is not found, set it to null
+        null
+    }
+
     @EventHandler
     fun onInventoryClose(event: InventoryCloseEvent) {
+        // Get the inventory title
+        val inventoryTitle = ChatColor.translateAlternateColorCodes('&', plugin.config.getString("trash.inventoryTitle", "Trash bin").toString())
+        val title = getTitleMethod?.invoke(event.view) as? String ?: event.view.title
         // Clear any remaining items in the trash inventory when closed
-        val inventoryTitle = plugin.config.getString("trash.inventoryTitle", "Trash bin")
-        if (event.view.title == inventoryTitle.toString()) {
+        if (title == inventoryTitle) {
             event.inventory.clear()
         }
     }
